477f3dc4a014bbf079638c82b0f113bd73235ddd
Brianna Sookhoo <briannasookhoo@Briannas-Air.attlocal.net>
2020-07-17 07:23:25 -0700

    adding build folder

---
+++ client/build/precache-manifest.a38f2660cddc51e4fe5bc53baa761abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763e5321d2804c22554b3d1296e291fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10309a3e5e36f57db6eb",
+    "url": "/static/css/main.c6d1ad78.chunk.css"
+  },
+  {
+    "revision": "a010ff0477437eb20d54",
+    "url": "/static/js/2.7d6c17a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d6c17a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10309a3e5e36f57db6eb",
+    "url": "/static/js/main.3d432b99.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
